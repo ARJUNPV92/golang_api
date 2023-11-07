e2b04c0daea9563c9a94535d521379d8f3befa2b
Arjun <arjunpv09@gmail.com>
2023-11-07 17:54:33 +0530

    Updated GORM tags in Stock model

--- models/stocks_model.go
+++ models/stocks_model.go
@@ -1,34 +1,34 @@
 package models
 
 import (
-    "time"
+	"time"
 )
 
 type Stock struct {
-    ID               uint      `gorm:"primary_key" json:"id"`
-    Timestamp        time.Time `json:"TIMESTAMP"`
-    ScCode           float64   `json:"ScCode"`
-    ScName           string    `json:"ScName"`
-    Symbol           string    `json:"SYMBOL"`
-    Open             float64   `json:"OPEN"`
-    High             float64   `json:"HIGH"`
-    Low              float64   `json:"LOW"`
-    Close            float64   `json:"CLOSE"`
-    Last             float64   `json:"Last"`
-    PrevClose        float64   `json:"PREVCLOSE"`
-    Series           string    `json:"SERIES"`
-    BSE_NSE_TotalTrades float64   `json:"BSE+NSE_TotalTrades"`
-    BSE_NSE_Vol        float64   `json:"BSE+NSE_Vol"`
-    BSE_NSE_DeliVol    float64   `json:"BSE+NSE_DeliVol"`
-    BSE_NSE_Deli       float64   `json:"BSE+NSE_Deli%"`
-    BSE_NSE_Turnover   float64   `json:"BSE+NSE_TurnoverRsCr"`
-    BSE_NSE_DeliVal    float64   `json:"BSE+NSE_DeliValRsCr"`
-    BSE_NSE_PerOrder   float64   `json:"BSE+NSE_PerOrderWorhRs"`
-    BSE_NSE_PerOrderQty float64   `json:"BSE+NSE_PerOrderQty"`
-    BSE_NSE_AvgPrice    float64   `json:"BSE+NSE_AvgPrice"`
-    ISIN              string    `json:"ISIN"`
+	ID               uint      `gorm:"primary_key" json:"id"`
+	TIMESTAMP        time.Time `gorm:"column:TIMESTAMP" json:"TIMESTAMP"`
+	ScCode           float64   `gorm:"column:ScCode" json:"ScCode"`
+	ScName           string    `gorm:"column:ScName" json:"ScName"`
+	SYMBOL           string    `gorm:"column:SYMBOL" json:"SYMBOL"`
+	OPEN             float64   `gorm:"column:OPEN" json:"OPEN"`
+	HIGH             float64   `gorm:"column:HIGH" json:"HIGH"`
+	LOW              float64   `gorm:"column:LOW" json:"LOW"`
+	CLOSE            float64   `gorm:"column:CLOSE" json:"CLOSE"`
+	Last             float64   `gorm:"column:Last" json:"Last"`
+	PREVCLOSE        float64   `gorm:"column:PREVCLOSE" json:"PREVCLOSE"`
+	SERIES           string    `gorm:"column:SERIES" json:"SERIES"`
+	BSENSETotalTrades float64 `gorm:"column:BSE+NSE_TotalTrades" json:"BSE+NSE_TotalTrades"`
+	BSENSEVol        float64   `gorm:"column:BSE+NSE_Vol" json:"BSE+NSE_Vol"`
+	BSENSEDeliVol    float64   `gorm:"column:BSE+NSE_DeliVol" json:"BSE+NSE_DeliVol"`
+	BSENSEDeli       float64   `gorm:"column:BSE+NSE_Deli%" json:"BSE+NSE_Deli%"`
+	BSENSETurnover   float64   `gorm:"column:BSE+NSE_TurnoverRsCr" json:"BSE+NSE_TurnoverRsCr"`
+	BSENSEDeliVal    float64   `gorm:"column:BSE+NSE_DeliValRsCr" json:"BSE+NSE_DeliValRsCr"`
+	BSENSEPerOrder   float64   `gorm:"column:BSE+NSE_PerOrderWorhRs" json:"BSE+NSE_PerOrderWorhRs"`
+	BSENSEPerOrderQty float64 `gorm:"column:BSE+NSE_PerOrderQty" json:"BSE+NSE_PerOrderQty"`
+	BSENSEAvgPrice    float64 `gorm:"column:BSE+NSE_AvgPrice" json:"BSE+NSE_AvgPrice"`
+	ISIN              string    `gorm:"column:ISIN" json:"ISIN"`
 }
 
 func (Stock) TableName() string {
-    return "tbl_BSE_NSE_Historic_Data"
+	return "tbl_BSE_NSE_Historic_Data"
 }
